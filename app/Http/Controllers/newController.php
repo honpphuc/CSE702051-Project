aeaa80f52d77d1be6cdcdb126af93a3178c0eeaa
nvluong-05 <izydragon1703@gmail.com>
2025-06-03 13:20:19 +0700

    commit

---
+++ app/Http/Controllers/newController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\News;
+use App\Models\User;
+
+class newController extends Controller
+{
+    //
+}
---
+++ database/seeders/NewsSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\News; // Dung de truy cap model News
+
+class NewsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
