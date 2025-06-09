6d2c8d7c5bbe2724abe554aa150100b81eeabbfe
Nguyen Van Luong <izydragon1703@gmail.com>
2025-06-09 16:31:19 +0700

    route toi login va register

--- resources/views/profile/partials/header.blade.php
+++ resources/views/profile/partials/header.blade.php
@@ -12,8 +12,10 @@
         <a href="#" class="text-gray-800 font-medium hover:text-primary transition">Liên hệ</a>
       </nav>
       <div class="flex items-center space-x-4">
-        <button id="loginBtn" class="text-gray-800 hover:text-primary transition font-medium">Đăng nhập</button>
-        <button id="registerBtn" class="bg-primary text-white py-2 px-4 rounded-button hover:bg-opacity-90 transition whitespace-nowrap">Đăng ký</button>
+        <button id="loginBtn" class="text-gray-800 hover:text-primary transition font-medium">
+          <a href="{{route ('login') }}">Đăng nhập</a></button>
+        <button id="registerBtn" class="bg-primary text-white py-2 px-4 rounded-button hover:bg-opacity-90 transition whitespace-nowrap">
+          <a href="{{route ('register') }}">Đăng ký</a></button>
         <button class="md:hidden w-10 h-10 flex items-center justify-center">
           <i class="ri-menu-line text-xl"></i>
         </button>
