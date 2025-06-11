316dfc4edbe1249fae66c26bcb53a70c51a048ed
Nguyen Van Luong <izydragon1703@gmail.com>
2025-06-12 00:53:53 +0700

    Cập nhật lại login và register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,7 +17,7 @@
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <x-input-label for="password" :value="__('Mật khẩu')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
                 type="password"
@@ -31,27 +31,27 @@
         <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
                 <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ml-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
+                <span class="ml-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Ghi nhớ mật khẩu') }}</span>
             </label>
         </div>
 
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
             <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                {{ __('Forgot your password?') }}
+                {{ __('Quên mật khẩu?') }}
             </a>
             @endif
 
             <x-primary-button class="ml-3">
-                {{ __('Log in') }}
+                {{ __('Đăng nhập') }}
             </x-primary-button>
         </div>
 
         <div class="flex items-center justify-center mt-4">
             <span class="text-sm text-gray-600 dark:text-gray-400">
-                Don't have an account?
+                Chưa có tài khoản?
                 <a class="underline text-indigo-600 dark:text-indigo-400 hover:text-indigo-500 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('register') }}">
-                    {{ __('Sign up here') }}
+                    {{ __('Đăng kí ở đây') }}
                 </a>
             </span>
         </div>
